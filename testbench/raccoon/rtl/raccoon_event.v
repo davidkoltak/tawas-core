89ae808973d0598b0c006aedec05b1d67104bf87
David Koltak <davidkoltak@gmail.com>
2016-11-23 00:40:04 -0600

    Adding new rule that raccoon bus id 0xff is reserved for anonomous slaves to send messages. Event module will handle this case.

--- testbench/raccoon/rtl/raccoon_event.v
+++ testbench/raccoon/rtl/raccoon_event.v
@@ -5,8 +5,12 @@
 //     David Koltak  11/22/2016
 //
 // This module provides four registers with 32 single bit flags per register.
-// Writing a value of (0-31) to a register sets it's flag. Reading a register
-// returns flag bit values and clears them.
+// Writing a value of (0-31) to a register sets a flag. Reading a register
+// returns the flag bit values and clears it.
+//
+// Using a bus ID of 8'hFF will cause this module to take the transaction off
+// the bus without acknowledge.  This is used for slave only peripherals that
+// are not assigned a bus ID.
 //
 // The MIT License (MIT)
 // 
@@ -70,7 +74,7 @@ module raccoon_event
       din <= RaccIn;
       addr_match_d1 <= addr_match;
       din_d1 <= din;
-      dout <= (addr_match_d1) ? {din_d1[79:78], 2'b10, din_d1[75:64], event_out[31:0], din_d1[31:0]} : din_d1[79:0];
+      dout <= (addr_match_d1) ? (din_d1[75:68] == 8'hFF) ? 80'd0 : {din_d1[79:78], 2'b10, din_d1[75:64], event_out[31:0], din_d1[31:0]} : din_d1[79:0];
     end
   
   reg [31:0] event_0;
