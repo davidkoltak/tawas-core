dcddac191096e05f347ff98ef0c0299c720536cd
davidkoltak <davidkoltak@gmail.com>
2017-06-08 22:15:20 -0500

    Forgot to increase sim max clocks.

--- projects/max10_devkit/testbench/testcase/string_test.mk
+++ projects/max10_devkit/testbench/testcase/string_test.mk
@@ -1,7 +1,7 @@
 
 TOOLS_PATH = ../../../../ip/tawas/tools/
 
-SIM_DEFS = -DMAX_CLOCKS=2000 -DSIM
+SIM_DEFS = -DMAX_CLOCKS=4000 -DSIM
 
 SRC = string_test.ta lib/string.ta lib/test.ta
 INC = -Iinclude/
